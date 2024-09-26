9bf7a3685e9e6f43ed5815f5ab6cca06c9cb8605
chenjinfan <fanchenjin@semi-tech.com>
2024-09-26 14:54:42 +0800

    first commit

---
+++ src/test/java/com/demo/springbox/SpringBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
